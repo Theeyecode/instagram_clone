f0fa24975ae24007aeb10a27dcff4cdc308fa160
Theeyecode <emmanuelolajubu90@gmail.com>
2023-02-28 10:26:11 +0100

    change file name

--- lib/state/image_upload/models/thumbnail_request.dart
+++ lib/state/image_upload/models/thumbnail_request.dart
@@ -9,4 +9,15 @@ class ThumbnailRequest {
   final FileType fileType;
 
   const ThumbnailRequest({required this.file, required this.fileType});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ThumbnailRequest &&
+          runtimeType == other.runtimeType &&
+          file == other.file &&
+          fileType == other.fileType;
+
+  @override
+  int get hashCode => Object.hashAll([runtimeType, file, fileType ]);
 }
---
+++ lib/views/components/file_thumbnail_view.dart
@@ -0,0 +1,42 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone_course/state/image_upload/models/thumbnail_request.dart';
+import 'package:instagram_clone_course/state/image_upload/providers/thumbnail_provider.dart';
+import 'package:instagram_clone_course/views/components/animations/loading_animation_view.dart';
+import 'package:instagram_clone_course/views/components/animations/small_errror_animation_view.dart';
+
+
+class FileThumbnailView extends ConsumerWidget {
+  final ThumbnailRequest thumbnailRequest;
+
+  const FileThumbnailView({
+    Key? key,
+    required this.thumbnailRequest,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final thumbnail = ref.watch(
+      thumbnailProvider(
+     thumbnailRequest,
+      ),
+    );
+
+    return thumbnail.when(
+      data: (imageWithAspectRatio) {
+        return AspectRatio(
+          aspectRatio: imageWithAspectRatio.aspectRatio,
+          child: imageWithAspectRatio.image,
+        );
+      },
+      loading: () {
+        return const LoadingAnimationView();
+      },
+      error: (error, stackTrace) {
+        return const SmallErrorAnimationView();
+      },
+    );
+  }
+}
